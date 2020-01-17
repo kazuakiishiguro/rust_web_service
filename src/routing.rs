a30d725834547a5e081093c42eb9e0e170a58a6a
Kazuaki Ishiguro <kazuaki@couger.co.jp>
2020-01-18 00:41:02 +0900

    update Handler object type

--- src/routing.rs
+++ src/routing.rs
@@ -1,19 +1,24 @@
 use std::collections::HashMap;
 
-use iron::{Handler};
-use iron::status;
 use iron::prelude::*;
+use iron::status;
+use iron::Handler;
 
 pub struct Router {
-    routes: HashMap<String, Box<Handler>>
+    routes: HashMap<String, Box<dyn Handler>>,
 }
 
 impl Router {
     pub fn new() -> Self {
-        Router { routes: HashMap::new() }
+        Router {
+            routes: HashMap::new(),
+        }
     }
 
-    pub fn add_route<H>(&mut self, path: String, handler: H) where H: Handler {
+    pub fn add_route<H>(&mut self, path: String, handler: H)
+    where
+        H: Handler,
+    {
         self.routes.insert(path, Box::new(handler));
     }
 }
@@ -22,7 +27,7 @@ impl Handler for Router {
     fn handle(&self, req: &mut Request) -> IronResult<Response> {
         match self.routes.get(&req.url.path().join("/")) {
             Some(handler) => handler.handle(req),
-            None => Ok(Response::with(status::NotFound))
+            None => Ok(Response::with(status::NotFound)),
         }
     }
 }
